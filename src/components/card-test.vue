5c7575d429818fdb1a6039045a2112fabf93b565
“nreupomckinsey” <naldo_reupo-musayon@mckinsey.com>
2023-04-11 20:11:06 -0500

    feat(PrimeVue) add prime framework

--- src/components/card-test.vue
+++ src/components/card-test.vue
@@ -1,9 +1,16 @@
 <template>
   <div class="my-class">My component {{name}}</div>
+  <button>Click me!</button>
+  <pv-button label="click me!" rounded class="my-class"/>
+  <input type="text">
+  <label for="username">Username</label>
+  <pv-input-text />
+  <pv-check-box />
 </template>
 
 <script>
 export default {
+
     props: ['name'],
     name: "card-test",
     mounted() {
@@ -14,7 +21,7 @@ export default {
 
 <style scoped>
   .my-class{
-      background: blue;
+      //background: blue;
       color: white;
   }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,26 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+
 import './assets/main.css'
 
-createApp(App).mount('#app')
+//PrimeVue
+import PrimeVue from 'primevue/config';
+//import "primevue/resources/themes/lara-light-indigo/theme.css"; //theme
+//import "primevue/resources/themes/arya-orange/theme.css";
+import "primevue/resources/themes/bootstrap4-light-blue/theme.css"
+import "primevue/resources/primevue.min.css"; //core
+import "primeicons/primeicons.css"; //icons
+import Button from 'primevue/button';
+import InputText from 'primevue/inputtext';
+import Checkbox from 'primevue/checkbox';
+
+const app = createApp(App);
+
+//inyectamos
+app.component('pv-button', Button);
+app.component('pv-input-text', InputText);
+app.component('pv-check-box',Checkbox)
+app.use(PrimeVue);
+
+app.mount('#app');
